436c469c6e672108669ef8cebc8bddf1770d12e4
FranciscoDiazPaccot73 <fran.diazpaccot@gmail.com>
2024-12-02 11:37:20 -0300

    feat: add gallery

---
+++ src/pages/about/pictures.js
@@ -0,0 +1,38 @@
+export const PICTURES = {
+  puestaSolMuelleSF: {
+    src: "https://images.pexels.com/photos/22042415/pexels-photo-22042415/free-photo-of-puesta-de-sol-muelle-argentina-santa-fe.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  },
+  faroSF: {
+    src: "https://images.pexels.com/photos/22042400/pexels-photo-22042400/free-photo-of-puesta-de-sol-faro-argentina-santa-fe.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  },
+  mcSF: {
+    src: "https://images.pexels.com/photos/29610681/pexels-photo-29610681.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  },
+  graffiti: {
+    src: "https://images.pexels.com/photos/29610684/pexels-photo-29610684.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  },
+  bridgeAndSculturebnw: {
+    src: "https://images.pexels.com/photos/29610773/pexels-photo-29610773.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  },
+  pnaStairs: {
+    src: "https://images.pexels.com/photos/29610825/pexels-photo-29610825.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  },
+  staFeStreet: {
+    src: "https://images.pexels.com/photos/29610677/pexels-photo-29610677.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  },
+  bridgeAtNight: {
+    src: "https://images.pexels.com/photos/29610685/pexels-photo-29610685.jpeg",
+  },
+  pcPool: {
+    src: "https://images.pexels.com/photos/29610824/pexels-photo-29610824.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  },
+  gamingKeyboard: {
+    src: "https://images.pexels.com/photos/29610771/pexels-photo-29610771.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  },
+  sfStreetBv: {
+    src: "https://images.pexels.com/photos/29610682/pexels-photo-29610682.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  },
+  meTakingAPhoto: {
+    src: "https://images.pexels.com/photos/29610679/pexels-photo-29610679.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  },
+};
